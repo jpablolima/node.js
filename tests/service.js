af7a35ea44669a3f0cdcad27c847dfe0c911c85e
João Pablo <jp.pablolima@gmail.com>
2019-09-17 11:27:16 -0300

    modulos de teste - Nock

--- tests/service.js
+++ tests/service.js
@@ -5,7 +5,15 @@ const URL = `https://swapi.co/api/people`
 async function obterPessoas(nome) {
   const url = `${URL}/?search=${nome}&format=json`
   const result = await get(url)
-  return result.data
+  //console.log(result.data)
+  return result.data.results.map(mapearPessoas)
 }
+function mapearPessoas(item) {
+  return {
+    nome: item.name,
+    peso: item.height
+  }
+}
+
 
 module.exports = { obterPessoas }
